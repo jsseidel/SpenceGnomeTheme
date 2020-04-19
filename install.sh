a19930f961e04f518aba5013f31db4b1e92b3dfd
jsseidel <jsseidel@fastmail.com>
2020-04-19 18:46:39 -0400

    Added something to reduce window title size.

--- install.sh
+++ install.sh
@@ -55,6 +55,8 @@ gsettings set org.gnome.desktop.wm.preferences titlebar-font 'San Francisco Disp
 gsettings set org.gnome.nautilus.desktop font 'San Francisco Display 11'
 #gsettings reset org.gnome.desktop.interface text-scaling-factor
 
+cp -v config_gtk-3.0_gtk.css ~/.config/gtk-3.0/gtk.css
+
 set +x
 
 echo "Done."
